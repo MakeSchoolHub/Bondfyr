7fc4714f4034ec187f72d9cce6949981139408c7
Muhammad Tifak <moht20009@gmail.com>
2020-12-05 23:57:11 -0500

    Creating registration form

--- react-slack-clone/src/components/Auth/Register.js
+++ react-slack-clone/src/components/Auth/Register.js
@@ -1,8 +1,49 @@
 import React from "react";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from "semantic-ui-react";
 
 class Register extends React.Component {
   render() {
-    return <div>Register</div>;
+    return (
+      <Grid textAlign="center" verticalAlign="middle">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for Bondfyr Lounge
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+            </Segment>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
